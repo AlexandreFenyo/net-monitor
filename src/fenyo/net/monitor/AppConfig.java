195c484c32a341be161b4d3cb8bb00790a1e6d28
AlexandreFenyo <github@fenyo.net>
2017-10-02 20:10:42 +0200

    new

--- src/fenyo/net/monitor/AppConfig.java
+++ src/fenyo/net/monitor/AppConfig.java
@@ -3,6 +3,9 @@ package fenyo.net.monitor;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+// test
+
+
 @Configuration
 @ComponentScan("fenyo.net.monitor")
 public class AppConfig {}
