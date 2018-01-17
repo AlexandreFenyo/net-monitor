94bf8e02feb2b6e6666047120fa7fdd7da4d21e6
fenyo <fenyo@W7>
2018-01-18 00:10:35 +0100

    ok

--- src/fenyo/net/monitor/WebController.java
+++ src/fenyo/net/monitor/WebController.java
@@ -10,6 +10,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -28,6 +29,7 @@ public class WebController {
     }
     
     @MessageMapping("/canStart")
+    @SendTo("/test/queue")
     public String handle(String greeting) {
         logger.debug("canStart: " + greeting.toString());
         return greeting;
