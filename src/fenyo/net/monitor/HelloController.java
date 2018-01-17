e3f25614eb96e0dd8ce3c1c10c9b6fe653caa1de
fenyo <fenyo@w69025595.ilc.eic.intra>
2018-01-17 09:28:26 +0100

    msg

---
+++ src/fenyo/net/monitor/HelloController.java
@@ -0,0 +1,15 @@
+package fenyo.net.monitor;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+//@Controller
+//public class HelloController {
+//
+//    @GetMapping("/hello")
+//    public String handle(final Model model) {
+//        model.addAttribute("message", "Hello World!");
+//        return "index";
+//    }
+//}
