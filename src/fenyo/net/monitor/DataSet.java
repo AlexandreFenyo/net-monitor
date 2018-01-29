77b80eb892ccbe2bc959a6ee302a1cbd6c377958
fenyo <fenyo@w69025595.ilc.eic.intra>
2018-01-29 19:39:36 +0100

    msg

---
+++ src/fenyo/net/monitor/DataSet.java
@@ -0,0 +1,63 @@
+package fenyo.net.monitor;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.Iterator;
+import java.util.List;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
+import java.time.temporal.TemporalUnit;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DataSet {
+    private static final Logger logger = LoggerFactory.getLogger(DataSet.class);
+    private final long time_window;
+    
+    private final List<Instant> instants = new ArrayList<Instant>();
+    private final List<String> values = new ArrayList<String>();
+
+    public DataSet(long time_window) {
+        this.time_window = time_window;
+    }
+
+    public synchronized Instant addValue(final String value) {
+        final Instant now = Instant.now();
+        instants.add(now);
+        values.add(value);
+        return now;
+    }
+
+    // keep at most only one value outside of the time window
+    public synchronized void flush() {
+        final Instant beginning = Instant.now().minus(time_window, ChronoUnit.SECONDS);
+        while (true) {
+            final Iterator<Instant> it = instants.iterator();
+            if (!it.hasNext()) return;
+            it.next();
+            if (!it.hasNext()) return;
+            if (it.next().isBefore(beginning)) {
+                instants.remove(0);
+                values.remove(0);
+            }
+            return;
+        }
+    }
+
+    public synchronized String toString() {
+        final Instant now = Instant.now();
+        final StringBuffer result = new StringBuffer("[");
+        final Iterator<String> it = values.iterator();
+        boolean first = true;
+        for (final Instant instant : instants) {
+            if (first) {
+                result.append(",");
+                first = false;
+            }
+            result.append("{\"time\":" + instant.until(now, ChronoUnit.SECONDS) + ",\"value\":" + it.next() + "}");
+        }
+        result.append("]");
+        return result.toString();
+    }
+}
--- src/fenyo/net/monitor/WebController.java
+++ src/fenyo/net/monitor/WebController.java
@@ -3,6 +3,8 @@ package fenyo.net.monitor;
 import java.net.URL;
 import java.nio.charset.Charset;
 import java.security.Principal;
+import java.time.Instant;
+import java.time.temporal.ChronoUnit;
 import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 
@@ -16,27 +18,44 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.view.RedirectView;
 
-@Controller
+@RestController
 public class WebController {
     private static final Logger logger = LoggerFactory.getLogger(WebController.class);
     private SimpMessagingTemplate template;
 
+    private DataSet data1 = new DataSet(120);
+    
     @Autowired
 	public WebController(final SimpMessagingTemplate template) {
 		this.template = template;
 	}
-    
-    @RequestMapping(value = "/request", method = RequestMethod.GET)
-    public ModelAndView user(final Principal p) {
-        logger.debug("salut");
 
-        String text = "date:valeur";
-		template.convertAndSend("/data/queue", text);
-
-		final ModelAndView mav = new ModelAndView("user");
+    // http://localhost:8080/net-monitor/dispatch/add?value=123
+    @RequestMapping(value = "/add", method = RequestMethod.GET)
+    public ModelAndView add(final Principal p, @RequestParam("value") final long value) {
+        data1.addValue(new Long(value).toString());
+        final String text = "{\"time\":0,\"value\":" + new Long(value).toString() + "}";
+        template.convertAndSend("/data/queue", text);
+        final ModelAndView mav = new ModelAndView("user");
         return mav;
+    }
+
+    @RequestMapping(value = "/request", method = RequestMethod.GET)
+    public Object request(final Principal p) {
+        class X {
+            public int a = 2;
+            public Integer getA() {
+                return 2;
+            }
+            public void setA(Integer a) {
+                
+            }
+        };
+        return new X();
     }
 }
