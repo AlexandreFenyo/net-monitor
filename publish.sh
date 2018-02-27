2a2a544cb2eebf1b4cf44bf962ff577089131459
Alexandre Fenyo <nospam@fenyo.net>
2018-02-27 18:41:58 +0100

    new

--- publish.sh
+++ publish.sh
@@ -1,4 +1,6 @@
 #!/bin/zsh
+
 npx webpack
 npm publish --access=public
+scp src/main/javascript/public/net-monitor.min.js fenyo@fenyo.net:public_html/eowyn.eu.org/cloudflare/net-monitor-$(jq -r .version < package.json).min.js
 
