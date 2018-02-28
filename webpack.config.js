6a367a48b58fb2cbbb8d85e6bfd29a8f116617c2
fenyo <fenyo@w69025595.ilc.eic.intra>
2018-02-28 07:14:01 +0100

    test

--- webpack.config.js
+++ webpack.config.js
@@ -49,22 +49,72 @@ let config_dev = {
 	plugins: []
 }
 
-let config_ie11 = {
-	entry: [ "babel-polyfill", "./src/main/javascript/src/net-monitor.js" ],
-	output: {
-		path: path.resolve(__dirname, "./src/main/javascript/public"),
-	    filename: "./net-monitor.dev.ie11.js",
-	    libraryTarget: 'var',
-		library: 'NetMonitor'
-	},
-	module: {
-		rules: [{
-			test: /\.js$/,
-			exclude: /node_modules/,
-			loader: "babel-loader"
-		}]
-	  },
-	plugins: []
-}
+//let config_ie11 = {
+//	entry: [ "babel-polyfill", "./src/main/javascript/src/net-monitor.js" ],
+//	output: {
+//		path: path.resolve(__dirname, "./src/main/javascript/public"),
+//	    filename: "./net-monitor.dev.ie11.js",
+//	    libraryTarget: 'var',
+//		library: 'NetMonitor'
+//	},
+//	module: {
+//		rules: [{
+//			test: /\.js$/,
+//			exclude: /node_modules/,
+//			loader: "babel-loader"
+//		}]
+//	  },
+//	plugins: []
+//}
+
+//let config_prod = {
+//	entry: [ "babel-polyfill", "./src/main/javascript/src/net-monitor.js" ],
+//	output: {
+//		path: path.resolve(__dirname, "./src/main/javascript/public"),
+//	    filename: "./net-monitor.min.js",
+//	    libraryTarget: 'var',
+//	    library: 'NetMonitor'
+//	},
+//	module: {
+//		rules: [{
+//			test: /\.js$/,
+//			exclude: /node_modules/,
+//			loader: "babel-loader"
+//		}]
+//	  },
+//	plugins: [
+//		new UglifyJsPlugin()
+//		]
+//}
+//
+//let config_dev = {
+//entry: [ "./src/main/javascript/src/net-monitor.js" ],
+//output: {
+//	path: path.resolve(__dirname, "./src/main/javascript/public"),
+//    filename: "./net-monitor.dev.js",
+//    libraryTarget: 'var',
+//    library: 'NetMonitor',
+//},
+//module: {},
+//plugins: []
+//}
+//
+//let config_ie11 = {
+//entry: [ "babel-polyfill", "./src/main/javascript/src/net-monitor.js" ],
+//output: {
+//	path: path.resolve(__dirname, "./src/main/javascript/public"),
+//    filename: "./net-monitor.dev.ie11.js",
+//    libraryTarget: 'var',
+//	library: 'NetMonitor'
+//},
+//module: {
+//	rules: [{
+//		test: /\.js$/,
+//		exclude: /node_modules/,
+//		loader: "babel-loader"
+//	}]
+//  },
+//plugins: []
+//}
 
  module.exports = [ config_prod, config_dev /* , config_ie11 */ ];
