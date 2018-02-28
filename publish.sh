6356d38cc3e326ca361f343cb1fdd892b18e4797
Alexandre Fenyo <fenyo@srv0.sec.le-rsi.net>
2018-02-28 18:06:13 +0100

    new

--- publish.sh
+++ publish.sh
@@ -1,9 +1,15 @@
 #!/bin/zsh
 
 echo "do not forget to update version in package.json prior to running this script"
+net-monitor.bundle.dev.js  net-monitor.bundle.min.js  net-monitor.standalone.dev.js  net-monitor.standalone.min.js
 
 npx webpack
 npm publish --access=public
-scp src/main/javascript/public/net-monitor.min.js fenyo@fenyo.net:public_html/eowyn.eu.org/cloudflare/net-monitor-$(jq -r .version < package.json).min.js
-ssh fenyo@fenyo.net "cd public_html/eowyn.eu.org/cloudflare/; rm -f net-monitor-latest.min.js ; ln -s "net-monitor-$(jq -r .version < package.json).min.js" net-monitor-latest.min.js"
-
+for b in bundle standalone
+do
+  for d in dev min
+  do
+    scp src/main/javascript/public/net-monitor.$b.$d.js fenyo@fenyo.net:public_html/eowyn.eu.org/cloudflare/net-monitor-$(jq -r .version < package.json).$b.$d.js
+    ssh fenyo@fenyo.net "cd public_html/eowyn.eu.org/cloudflare/; rm -f net-monitor-latest.$b.$d.js ; ln -s net-monitor-$(jq -r .version < package.json).$b.$d.js net-monitor-latest.$b.$d.js"
+  done
+done
