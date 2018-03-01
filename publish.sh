a7c5e0d694b87ee5d89221a0290f47540e38bf34
Alexandre Fenyo <fenyo@srv0.sec.le-rsi.net>
2018-03-01 14:27:40 +0100

    new

--- publish.sh
+++ publish.sh
@@ -2,6 +2,7 @@
 
 echo "do not forget to run ssh -L 2222:localhost:22 fenyo.net"
 echo "do not forget to update version in package.json prior to running this script"
+echo "do not forget to run mvn clean install"
 # net-monitor.bundle.dev.js  net-monitor.bundle.min.js  net-monitor.standalone.dev.js  net-monitor.standalone.min.js
 
 npx webpack
@@ -12,6 +13,7 @@ do
   do
     echo $b.$d
     scp -P 2222 src/main/javascript/public/net-monitor.$b.$d.js fenyo@localhost:public_html/eowyn.eu.org/cloudflare/net-monitor-$(jq -r .version < package.json).$b.$d.js
+    scp -P 2222 target/net-monitor-*-SNAPSHOT.war fenyo@localhost:public_html/eowyn.eu.org/cloudflare/net-monitor-$(jq -r .version < package.json).war
     ssh -p 2222 fenyo@localhost "cd public_html/eowyn.eu.org/cloudflare/; rm -f net-monitor-latest.$b.$d.js ; ln -s net-monitor-$(jq -r .version < package.json).$b.$d.js net-monitor-latest.$b.$d.js"
   done
 done
