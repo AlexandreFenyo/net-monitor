3549f2996699f6d83df8f17ee9697d1aa5d7aae0
Alexandre Fenyo <fenyo@srv0.sec.le-rsi.net>
2018-03-02 17:11:43 +0100

    new

---
+++ docker/net-monitor/startup.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+if [ ! -f /config.json ]
+then
+  echo you must specify a config.json file - see documentation
+   exit 1
+fi
+
+if [ ! -f /log4j.xml ]
+then
+  echo you must specify a log4j.xml file - see documentation
+  exit 1
+fi
+
+cp /config.json /net-monitor/target/net-monitor-*-SNAPSHOT/META-INF/config.json
+cp /log4j.xml /net-monitor/target/net-monitor-*-SNAPSHOT/WEB-INF/classes/log4j.xml
+
+mvn tomcat7:run
