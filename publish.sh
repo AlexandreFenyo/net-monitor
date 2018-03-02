e6b4e59cdd7a05cb4c9a453d8b28c494ec870191
Alexandre Fenyo <fenyo@srv0.sec.le-rsi.net>
2018-03-02 17:35:14 +0100

    new

--- publish.sh
+++ publish.sh
@@ -3,7 +3,6 @@
 echo "BEFORE RUNNING THIS SCRIPT:"
 echo "do not forget to run ssh -L 2222:localhost:22 fenyo.net"
 echo "do not forget to update version in package.json prior to running this script"
-echo "do not forget to run mvn clean install"
 echo "do not forget to commit and push the repo to GitHub"
 
 echo "sleeping 5 secs"
@@ -20,10 +19,13 @@ do
   do
     echo $b.$d
     scp -P 2222 src/main/javascript/public/net-monitor.$b.$d.js fenyo@localhost:public_html/eowyn.eu.org/cloudflare/net-monitor-$(jq -r .version < package.json).$b.$d.js
-    scp -P 2222 target/net-monitor-*-SNAPSHOT.war fenyo@localhost:public_html/eowyn.eu.org/cloudflare/net-monitor-$(jq -r .version < package.json).war
     ssh -p 2222 fenyo@localhost "cd public_html/eowyn.eu.org/cloudflare/; rm -f net-monitor-latest.$b.$d.js ; ln -s net-monitor-$(jq -r .version < package.json).$b.$d.js net-monitor-latest.$b.$d.js"
   done
 done
+
+rm -rf target
+mvn clean compile war:war
+scp -P 2222 target/net-monitor-*-SNAPSHOT.war fenyo@localhost:public_html/eowyn.eu.org/cloudflare/net-monitor-$(jq -r .version < package.json).war
 
 cd docker
 make build-net-monitor
