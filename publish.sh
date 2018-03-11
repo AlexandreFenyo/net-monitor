366843a55c6ec4c93a22ff6d89c7b035db519260
Alexandre Fenyo <alex@fenyo.net>
2018-03-11 21:01:10 +0100

    new

--- publish.sh
+++ publish.sh
@@ -4,6 +4,8 @@ echo "BEFORE RUNNING THIS SCRIPT:"
 echo "do not forget to run ssh -L 2222:localhost:22 fenyo.net"
 echo "do not forget to update version in package.json prior to running this script"
 echo "do not forget to commit and push the repo to GitHub (after having updated package.json)"
+echo "do not forget docker login"
+echo "do not forget npm adduser"
 
 echo "sleeping 5 secs"
 sleep 5
